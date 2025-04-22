0f314b1d0612c9135f3fce6656055e345b1459bd
femi <femiehindero@gmail.com>
2025-04-22 19:43:04 +0100

    Add .nvmrc for Node.js version 18.17.0 and update Next.js configuration to set output to standalone

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'], // Add any other image domains you're using
   },
+  output: 'standalone',
   // Remove experimental features that might cause issues
   experimental: {
     optimizeCss: false,
