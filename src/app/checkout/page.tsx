b14c0b6083511662436b068d0ef4a6d1d458aac4
femi <femiehindero@gmail.com>
2025-05-11 01:35:52 +0100

    feat: specify order intent for PayPal payment processing
    - Added intent set to "CAPTURE" in the order creation for PayPal to ensure immediate payment capture during checkout.

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -186,6 +186,7 @@ export default function CheckoutPage() {
                     style={{ layout: "vertical" }}
                     createOrder={(data, actions) => {
                       return actions.order.create({
+                        intent: "CAPTURE",
                         purchase_units: [{
                           amount: {
                               currency_code: "GBP",
